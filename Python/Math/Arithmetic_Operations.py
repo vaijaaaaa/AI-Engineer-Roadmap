f0ac39b29f550c086c5648546d66e8c8399ff07d
vaijaaaaa <vaijuwalker111@gmail.com>
2026-04-06 11:52:07 +0530

    Add arithmetic operations and math functions examples

---
+++ Python/Math/Arithmetic_Operations.py
@@ -0,0 +1,6 @@
+print(10+5)
+print(50-10)
+print(6*10)
+print(10/2)
+print(10%2)
+print(10//2) # floor division
---
+++ Python/Math/Math_Functions.py
@@ -0,0 +1,8 @@
+import math;
+
+print(math.factorial(5))
+print(math.isqrt(9))
+print(math.floor(2.4))
+print(math.ceil(2.4))
+print(math.pow(10,2))
+print(math.sqrt(10))
