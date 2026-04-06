62c46af1a070d30b3b8b83f0fc1a920dc6c8685c
vaijaaaaa <vaijuwalker111@gmail.com>
2026-04-06 11:32:36 +0530

    string Methods

---
+++ Python/Strings/Strings_methods.py
@@ -0,0 +1,9 @@
+course = "Python"
+print(len(course))
+
+print(course.upper())
+print(course.lower())
+print(course.find('P')) #index
+print(course.replace('P','J'))
+
+print('y' in course) #Finding the character present in the string variable
